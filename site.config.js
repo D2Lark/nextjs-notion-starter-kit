9f9d4c78ff3c66aebae6a0d2fa0e8ada1e914707
D2Lark <100247023+D2Lark@users.noreply.github.com>
2022-03-26 17:43:03 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 export default {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9f4930d673ac418eb91496ee56bb8185',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'D2Lark's Blog',
+  domain: 'https://www.notion.so/My-Blog-9f4930d673ac418eb91496ee56bb8185',
+  author: 'D2Lark',
 
   // open graph metadata (optional)
   description: 'Example site description',
